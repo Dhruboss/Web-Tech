e1fba8f759ca317f773b93c6fd1f35a32670309a
Salman <75027367+Dhruboss@users.noreply.github.com>
2021-04-07 15:44:38 +0600

    Add files via upload

---
+++ lab rask 6/Model/model.php
@@ -0,0 +1,82 @@
+<?php 
+
+require_once 'dbconnect.php';
+
+
+
+function adduser($data){
+	$conn = db_conn();
+    $selectQuery = "INSERT INTO userinfo (Name, Username ,Password, Email, DateOfBirth , Gender, Institution , Company, UserType, BloodGroup ) VALUES (:name, :Username, :Password,  :email, :dob, :gender, :institution, :company, :user, :blood  )";
+
+    try{
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([
+                     ':name'               =>     $data['name'],   
+                     ':uname'               =>     $data['Username'],  
+                     ':pass'          =>     $data["Password"], 
+                     ':email'          =>     $data["email"],
+                     ':dob'     =>     $data["dob"],  
+                     ':gender'          =>     $data["gender"],  
+                     ':institution'     =>     $data["institution"],  
+                      ':company'               =>     $data['company'],  
+                     ':user'          =>     $data["user"],  
+                     ':blood'     =>     $data["blood"] 
+
+        ]);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    
+    $conn = null;
+    return true;
+}
+
+function searchUser($user_name){
+    $conn = db_conn();
+    $selectQuery = "SELECT * FROM `userinfo` WHERE username = ?";
+
+    
+    try {
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([$user_name]);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $data;
+}
+
+
+function showData($id){
+    $conn = db_conn();
+    $selectQuery = "SELECT * FROM `userinfo` where ID = ?";
+    try {
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([$id]);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $data;
+}
+
+function updateData($id, $data){
+    $conn = db_conn();
+    $selectQuery = "UPDATE userinfo set Name = ?, Username = ? where ID = ?";
+    try{
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->execute([
+            $data['name'], $data['username'], $id
+        ]);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+    
+    $conn = null;
+    return true;
+}
+
+
+
+?>
